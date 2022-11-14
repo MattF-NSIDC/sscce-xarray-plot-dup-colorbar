4ac63c72a2cb4ccad64cb4d05d5a3ab78bdc29b1
Matt Fisher <matt.fisher@nsidc.org>
2022-11-14 12:10:45 -0700

    Improve comments

--- sscce_xarray_plot_dup_colorbar/util.py
+++ sscce_xarray_plot_dup_colorbar/util.py
@@ -2,20 +2,28 @@ import cartopy.crs as crs
 import matplotlib
 import xarray as xra
 
+# The app crashes on second load if the following setting is not applied.
+#     https://matplotlib.org/stable/users/explain/backends.html
+# Error:
+#     UserWarning: Starting a Matplotlib GUI outside of the main thread will likely
+#     fail.
 matplotlib.use('Agg')
 
 
 def make_plot():
-    """Plot air temp in a way that reproduces duplicate colorbar issue."""
+    """Plot air temp in a way that reproduces duplicate colorbar issue.
+
+    One additional colorbar will be added for each page load.
+    """
     air_temp = xra.tutorial.open_dataset("air_temperature").air.isel(time=0)
 
     plot = air_temp.plot(
         subplot_kws={
-            'projection': crs.Orthographic(-45, 90),
+            'projection': crs.Orthographic(-45, 75),
             'facecolor': "gray"
         },
         transform=crs.PlateCarree(),
-        # Uncomment to fix the behavior:
+        # Uncomment to fix the duplicate colorbar behavior:
         # figsize=(6, 6),
     )
     plot.axes.coastlines(resolution='110m', color='white', linewidth=0.5)
